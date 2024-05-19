5316ba79993cfdb59ea9c3386d154287d019b511
Decay0x <140253856+Decay0x@users.noreply.github.com>
2024-05-19 11:21:42 +0100

    Tidy up the mobile view

--- src/spinner.js
+++ src/spinner.js
@@ -2,7 +2,7 @@ export default function Spinner() {
   const body = document.querySelector('body');
   const spinnerLoader = document.createElement('div');
   spinnerLoader.className =
-    'loader absolute -top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10';
+    'loader absolute bottom-[50vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10';
   const spinner = {
     show: () => {
       body.appendChild(spinnerLoader);
